daa36c3511b48cee447678831aed96a087460e1f
marcoberdiano <68471261+marcoberdiano@users.noreply.github.com>
2022-05-29 14:19:42 +0100

    fix some bug

--- src/components/Ratings.jsx
+++ src/components/Ratings.jsx
@@ -7,39 +7,24 @@ const Ratings = ({onClick}) => {
     const [rating, setRating] = useState('');
    
     const handleClick = ({target}) => {
-        switch(target.innerText) {
-            case '1':
-                onClick('1')
-                setRating('1')        
-                break;
-            case '2':
-                onClick('2');
-                setRating('2')
-                break;
-            case '3':
-                onClick('3');
-                setRating('3')
-                break;
-            case '4':
-                onClick('4');
-                setRating('4')
-                break;
-            case '5':
-                onClick('5');
-                setRating('5')
-                break;
-            default:
-                break;
-        }
+        
+        onClick(target.innerText);
+        setRating(target.innerText);
+        
     }
     return ( 
         <>
             {ratings.map( (item, index) => 
-                rating === (index+1).toString() || '' ? <CircleAtive key={index} onClick={handleClick}>
-                <span>{item}</span>
-                </CircleAtive> :  <CircleRating key={index} onClick={handleClick}>
-                <span>{item}</span>
-                </CircleRating>
+                
+                rating === (index+1).toString() || '' 
+                
+                ?   <CircleAtive key={index} onClick={handleClick}>
+                        <span>{item}</span>
+                    </CircleAtive> 
+                
+                :   <CircleRating key={index} onClick={handleClick}>
+                        <span>{item}</span>
+                    </CircleRating>
             )}
         </>);
 }
